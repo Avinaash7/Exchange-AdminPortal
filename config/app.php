4cf1398c2648a4da6774dd05e80cd00f74cd5471
Avinaash7 <mavinaash230102@gmail.com>
2023-08-29 00:44:25 +0530

    app

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    //'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://backend.yashkumarverma.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
